9cf6e9978bf2b5611a3e455ae38ba912fade2d06
Clarence Bermudez <csbermudez04@gmail.com>
2023-01-25 11:57:09 -0800

    Compile and fix Array bigs

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlaceMultiple() {
+    int[] input1 = { 3, 2, 1 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[] { 1, 2, 3 }, input1);
+  }
+
+  @Test
+  public void testReversedMultiple() {
+    int[] input1 = { 3, 2, 1 };
+    assertArrayEquals(new int[] { 1, 2, 3 }, ArrayExamples.reversed(input1));
   }
 }
